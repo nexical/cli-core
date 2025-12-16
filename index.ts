91241e543a7c4a135e490b02daf2907f5da3f766
Adrian Webb <adrian@webb.sh>
2025-12-16 00:16:22 -0500

    Adding configuration utilities to the global exports.

--- index.ts
+++ index.ts
@@ -3,5 +3,6 @@ export { BaseCommand } from './src/BaseCommand.js';
 
 export { logger, setDebugMode } from './src/utils/logger.js';
 export { runCommand } from './src/utils/shell.js';
+export { findProjectRoot, loadConfig } from './src/utils/config.js';
 
 export * from './src/CommandInterface.js';
